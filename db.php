410f24969e621e7d5796887ae8818794b030d1ab
Ashna Ali <ashnaalipk@gmail.com>
2026-06-20 17:39:56 +0500

    Initial commit - restaurant management system

--- db.php
+++ db.php
@@ -1,7 +1,14 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "restaurant_db");
+// InfinityFree database connection details
+$db_host = "sql106.infinityfree.com";
+$db_user = "if0_42228787";
+$db_pass = "VIPkG2SyanFD";   // <-- yahan apna MySQL password daalein
+$db_name = "if0_42228787_restaurant_db";
+$db_port = 3306;
+
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
 
 if ($conn->connect_error) {
-    die("Connection failed");
+    die(json_encode(["error" => "Connection failed: " . $conn->connect_error]));
 }
 ?>
