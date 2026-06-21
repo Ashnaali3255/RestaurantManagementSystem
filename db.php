10d64dd857e5b051712b713d04df618e4078143f
Ashna Ali <ashnaalipk@gmail.com>
2026-06-21 16:04:26 +0500

    Use CORS proxy for production

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
-// InfinityFree database connection details
-$db_host = "sql106.infinityfree.com";
-$db_user = "if0_42228787";
-$db_pass = "VIPkG2SyanFD";   // <-- yahan apna MySQL password daalein
-$db_name = "if0_42228787_restaurant_db";
+// AwardSpace database connection details
+$db_host = "fdb29.awardspace.net";
+$db_user = "4769668_restdb";
+$db_pass = 'kPYj{(oq1NNm?NXK';   // single quotes used because password has special characters
+$db_name = "4769668_restdb";
 $db_port = 3306;
 
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
